e81d01c0492dfa747278767865fe691174cc601a
Tobias'j1 <tobiasismaeljara@fpuna.edu.py>
2024-08-25 16:52:54 -0400

    Se agrega la funcion de promedio de dos valores

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+int a;
+int b;
+
 void main (void) {
+
 	printf ("Hola, mundo");
+	
+	//Se pide la introduccion de dos valores y se los 
+	//asigna a las variables globales
+	printf("Introducir el 1er valor: ");
+	scanf("%d",&a);
+	
+	printf("Introducir el 2do valor: ");
+	scanf("%d",&b);
+
+	//Se imprime el valor del promedio
+	printf("El promedio de los dos valores es: \n%d",(float)((a+b)/2));
+
 	return 0;
 }
